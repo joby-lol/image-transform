8686f9ddd7f48e9440e7f6905c591618ed53b427
Joby Elliott <joby@byjoby.com>
2020-10-01 12:45:56 -0600

    starting to add tests

---
+++ tests/units/mock/MockDriver.php
@@ -0,0 +1,9 @@
+<?php
+/* image-transform | https://github.com/jobyone/image-transform | MIT License */
+namespace ByJoby\ImageTransform\tests\units\mock;
+
+use ByJoby\ImageTransform\Drivers\AbstractDriver;
+
+class MockDriver extends AbstractDriver
+{
+}
